f52e4277d8b8d88e33b52351dc2da0c6b9739328
octopus7 <octopus7net@gmail.com>
2018-09-08 11:26:42 +0900

    os별 드라이버패스 지정

--- bluehousebest.py
+++ bluehousebest.py
@@ -2,11 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 
-#pypath = os.path.dirname(os.path.abspath(__file__))
+pypath = os.path.dirname(os.path.abspath(__file__))
+print(pypath)
+
+chromepath = 'chromedriver'
+with open('drvpath.txt', 'r') as myfile:
+    chromepath = myfile.read()
 
 options = Options()
 options.add_argument('--headless')
-browser = webdriver.Chrome('chromedriver', chrome_options=options)
+browser = webdriver.Chrome(chromepath, chrome_options=options)
 
 for page in range(1,4):
     print("page : " + str(page))
@@ -14,7 +19,3 @@ for page in range(1,4):
     print(browser.title)
 
 browser.quit()
-
-
-
-
